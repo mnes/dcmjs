b3cbef8963ca3564d42fca3136b16049ffba5c2e
Jerome Sadou <sadou@mnes.org>
2024-07-10 11:24:33 +0900

    adding some attributes

--- build.js
+++ build.js
@@ -5,7 +5,12 @@ esbuild
     .build({
         entryPoints: ["src/index.js"],
         bundle: true,
-        outfile: "dist/dcmjs.js",
-        format: "esm"
+        outfile: "build/dcmjs.js",
+        format: "esm",
+        sourcemap: true,
+        minify: true,
+        target: ["es6"],
+        platform: "browser",
+        plugins: []
     })
     .catch(() => process.exit(1));
